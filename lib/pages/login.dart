e2ba9896e09ff811eab4a3dd9affa6f48b0f29a5
syahjamal <sulaimansyahjamal@gmail.com>
2019-12-27 16:49:06 +0700

    Lets start login with google, semangat !!!

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,31 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:sibetonapp/main.dart';
+
+//Button
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+//FireBase
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//Shared preferences & Flutter Toast
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 
 //LoginPage Bawaan
